4d38bc91b387478e851cfc9c0175eaff2685f9d3
joseph <heegu0311@gmail.com>
2024-11-21 00:40:42 +0900

    solve: leetcode 739

---
+++ leetcode_739_Daily_Temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        result = [0] * len(temperatures)
+        stack = []
+        for cur_day, cur_temp in enumerate(temperatures):
+            while stack and stack[-1][1] < cur_temp:
+                prev_day, _ = stack.pop()
+                result[prev_day] = cur_day - prev_day
+            stack.append((cur_day, cur_temp))
+        return result
+
+solution = Solution()
+print(solution.dailyTemperatures([73,74,75,71,69,72,76,73]))
