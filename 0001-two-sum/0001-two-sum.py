2aa91fb9af3e9749dd372cbd264a52aeff72340c
Yoon Hee-Ku <heegu0311@gmail.com>
2024-12-19 19:56:11 +0900

    Time: 2 ms (56.86%), Space: 17.2 MB (91.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if(nums[i] + nums[j] == target):
-                    return [i, j]
+        l = 0
+        r = n - 1
 
+        copy = nums.copy()
+        copy.sort()  # sort array for quick search O(n)
+        reverseCopy = nums.copy()  # copy array for search O(n)
+        reverseCopy.reverse()
 
-        
+        while l < r:
+            sumTwo = copy[l] + copy[r]
+
+            if sumTwo == target:
+                result = [nums.index(copy[l]), n - 1 - reverseCopy.index(copy[r])]
+                result.sort()
+                return result
+
+            if sumTwo < target:
+                # if(target is greater than sum), move start to the next index
+                l += 1
+            elif sumTwo > target:
+                # if(target is smaller than sum), move start to the prev index
+                r -= 1
